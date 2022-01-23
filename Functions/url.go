2066850265b52a67b2d044381d9adc0a96692b2f
chandrikadeb7 <chandrikadeb7@gmail.com>
2022-01-23 21:41:50 +0530

    comments added

--- Functions/url.go
+++ Functions/url.go
@@ -5,29 +5,30 @@ import (
 	"net/http"
 )
 
+// get header content type of request
 func contentType(url string) (string, error) {
-	resp, err := http.Get(url)
+	resp, err := http.Get(url) //get the URL
 	if err != nil {
-		return "Error in URL", err
+		return "Error in URL", err //error in get method
 	}
 
-	defer resp.Body.Close()
+	defer resp.Body.Close() //close the response body
 
-	ctype := resp.Header.Get("Content-Type")
+	ctype := resp.Header.Get("Content-Type") //get the content type
 	if ctype == " " {
-		return "", fmt.Errorf("Cannot find Content-Type header in URL")
+		return "", fmt.Errorf("Cannot find Content-Type header in URL") //error in content type
 	}
 
-	return ctype, nil
+	return ctype, nil //return the content type
 
 }
 
 func main() {
-	ctype, err := contentType("https://golang.org")
+	ctype, err := contentType("https://golang.org") //call the content type of URL
 
 	if err != nil {
-		fmt.Printf("Error %v\n", err)
+		fmt.Printf("Error %v\n", err) //error handling
 	} else {
-		fmt.Println(ctype)
+		fmt.Println(ctype) //content type output
 	}
 }
